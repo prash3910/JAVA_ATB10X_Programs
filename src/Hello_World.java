072561bf209fa4785e12ca596469c567617f3ea6
Prashant <prash.3910@gmail.com>
2025-01-19 09:52:25 +0530

    Greatest of 3

--- src/Hello_World.java
+++ src/Hello_World.java
@@ -2,12 +2,21 @@ public class Hello_World {
 
     public static void main(String[] args) {
     System.out.println("Hello");
+    int a= 15;
+    int b= 56;
+    int c= 34;
 
-    }
-
-    public static void main()
+    if(a>=b)
     {
-        System.out.println(" Hello Prashant");
+        if(a>c)
+            System.out.println("a is greatest"+ a);
+    } else if (b>c) {
+        System.out.println("b is greatest"+ b);
 
     }
+    else
+        System.out.println("c is greatest"+ c);
+    }
+
+
 }
